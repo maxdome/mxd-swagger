3b23e65eb86597eca761d68bcce818c3b0eadf11
gkaya <kaya.goeksel@maxdome.de>
2017-02-06 10:31:50 +0100

    YML Support: if swagger-yml exists, load it, else swagger.json FIX: /docs route added first before loading static files, hence returning Swagger UI with modified URL instead of default index.html of Swagger UI

--- src/lib.js
+++ src/lib.js
@@ -5,24 +5,32 @@ const path = require('path');
 const SwaggerParser = require('swagger-parser');
 
 module.exports = (app, express) => {
-  const api = require(`${process.cwd()}/config/swagger.json`);
-  SwaggerParser.validate(api, () => {
-    SwaggerParser.bundle(api, (err, schema) => {
-      let revision = '';
-      try {
-        revision = '-' + fs.readFileSync(`${process.cwd()}/REVISION`, 'utf-8');
-      } catch (e) {}
-      schema.info.version = require(`${process.cwd()}/package.json`).version + revision;
-      app.get('/api-docs', (req, res) => {
-        res.send(schema);
-      });
+  const api = fs.existsSync(`${process.cwd()}/config/swagger.yml`) ? `${process.cwd()}/config/swagger.yml` : require(`${process.cwd()}/config/swagger.json`);
+  SwaggerParser.validate(api)
+    .then(api => {
+      SwaggerParser.bundle(api)
+        .then(schema => {
+          let revision = '';
+          try {
+            revision = '-' + fs.readFileSync(`${process.cwd()}/REVISION`, 'utf-8');
+          } catch (e) {}
+          schema.info.version = require(`${process.cwd()}/package.json`).version + revision;
+          app.get('/api-docs', (req, res) => {
+            res.send(schema);
+          });
+        })
+        .catch(bundleError => {
+          console.log(bundleError);
+        });
+    })
+    .catch(validationError => {
+      console.log(validationError.message);
     });
-  });
   const swaggerUIDirname = path.dirname(require.resolve('swagger-ui/package.json'));
-  app.use('/docs', express.static(`${swaggerUIDirname}/dist`));
   let html = fs.readFileSync(`${swaggerUIDirname}/dist/index.html`, 'utf8');
   html = html.replace(/url = "(.*)"/, `url = window.location.protocol + '//' + window.location.host + '/api-docs'`);
   app.get('/docs', (req, res) => {
     res.send(html);
   });
+  app.use('/docs', express.static(`${swaggerUIDirname}/dist`));
 };
